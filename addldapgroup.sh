3780bee27d36f36d43dd48720f73ca54c4515028
leorick <leorick>
2016-06-16 14:39:16 +0800

    adding addldapgroup

---
+++ addldapgroup.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+# filename: addldapgroup.sh
+CONFIG="/root/ldap/config"
+. $CONFIG
+if [[ ( -z $1 ) || ( -z $2 ) ]]; then
+  echo "$0 <groupname> <type>, type = [1|2|3]"
+exit 1
+fi
+GROUPNAME=$1
+TYPE=$2
+LDIFNAME=$TMP/grp_$GROUPNAME.ldif
+LGID=`echo $[ 20000 + $[ RANDOM % 9999 ]]`
+(
+cat <<add-group
+dn: cn=$GROUPNAME,ou=group,dc=chttl,dc=cht,dc=com,dc=tw
+objectClass: posixGroup
+description: type$TYPE
+gidNumber: $LGID
+cn: $GROUPNAME
+add-group
+) > $LDIFNAME
+$LDAPADDCMD -x -w $LDAPPASS -D "cn=root,dc=chttl,dc=cht,dc=com,dc=tw" -f $LDIFNAME
+if [ $? -ne "0" ]; then
+ echo "Failed"
+ echo "Please review $LDIFNAME and add the account manually"
+else
+ echo "Successfully, gidNumber=$LGID"
+fi
--- addldapuser.sh
+++ addldapuser.sh
@@ -1,73 +1,38 @@
 #!/bin/sh
-
-
-# Script to add a login to ldap. 
-# Not meant for anything beyound casual use
-# Author: Rilindo Foster
-# Contact:  rilindo.foster@monzell.com
-# Date: 09/11/2011
-
+# filename: addldapuser.sh
 CONFIG="/root/ldap/config"
-
 . $CONFIG
-
-if [ -z $1 ];  then
-	echo "addldapuser.sh <username>"
-	exit 1
+if [[ ( -z $1 ) || ( -z $2 ) ]]; then
+  echo "addldapuser.sh <gidNumber> <username> "
+  exit 1
 fi
-
 USERNAME=$1
-
-stty_orig=`stty -g`
-echo -n "Enter Password: "
-stty -echo
-read USERPASS
-stty $stty_orig
-
-echo
-
-
-PASSWORD=`$SLAPPASSWORD -h "{crypt}" -s $USERPASS`
-
-# Generate a ID between 1000 and 65535. Its okay for home and maybe a small offie. For a medium to large company, you need a bigger number or try something different.
-
-LUID=`echo $[ 1000 + $[ RANDOM % 65535 ]]`
-
+LDIFNAME=$TMP/usr_$USERNAME.ldif
+LGID=$2
+LUID=`echo $[ 20000 + $[ RANDOM % 9999 ]]`
+PASSWORD=`$SLAPPASSWORD -h "{crypt}" -s $LUID`
 (
 cat <<add-user
-dn: uid=$USERNAME,ou=People,dc=$DOMAIN,dc=$ORG
+dn: uid=$USERNAME,ou=people,dc=chttl,dc=cht,dc=com,dc=tw
+objectClass: posixAccount
+objectClass: inetOrgPerson
+objectClass: organizationalPerson
+objectClass: person
+homeDirectory: /home/$USERNAME
+loginShell: /bin/bash
+gidNumber: $LGID
+uidNumber: $LUID
 uid: $USERNAME
 cn: $USERNAME
-objectClass: account
-objectClass: posixAccount
-objectClass: top
-objectClass: shadowAccount
+sn: $USERNAME
 userPassword: $PASSWORD
-shadowLastChange: 15192
-shadowMin: 0
-shadowMax: 99999
-shadowWarning: 7
-loginShell: /bin/bash
-uidNumber: $LUID
-gidNumber: $LUID
-homeDirectory: /home/users/$USERNAME
-
-dn: cn=$USERNAME,ou=Group,dc=$DOMAIN,dc=$ORG
-objectClass: posixGroup
-objectClass: top
-cn: $USERNAME 
-userPassword: {crypt}x
-gidNumber: $LUID
+mail: $USERNAME@cht.com.tw
 add-user
-) > $TMP/adduser.ldif
-
-$LDAPADDCMD -x -w $LDAPPASS -D "cn=root,dc=$DOMAIN,dc=$ORG" -f $TMP/adduser.ldif && rm $TMP/adduser.ldif 
-
+) > $LDIFNAME
+$LDAPADDCMD -x -w $LDAPPASS -D "cn=root,dc=chttl,dc=cht,dc=com,dc=tw" -f $LDIFNAME
 if [ $? -ne "0" ]; then
-	echo "Add user failed"
-	echo "Please review $TMP/adduser.ldif and add the account manually"
+  echo "Failed"
+  echo "Please review $LDIFNAME and add the account manually"
 else
-#	mkdir -p $HOMEDIR/$USERNAME
-	cp -Rv $SKEL $HOMEDIR/$USERNAME
-	chown -Rv $LUID:$LUID $HOMEDIR/$USERNAME
+  echo "Successfully, gidNumber=$LGID, uidNumber=$LUID"
 fi
