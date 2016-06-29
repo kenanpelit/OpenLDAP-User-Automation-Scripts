7c066f8968e1d113a8626e37b7ff0a735a8496fa
leorick <leorick>
2016-06-29 17:02:08 +0800

    modifying type1 script

--- createproj_type1.sh
+++ createproj_type1.sh
@@ -13,15 +13,16 @@ DESCRIP="loader"
 #
 echo "+-------------------------------------------------------------------+"
 echo "This script will "
-echo "(1) Creating LDAP Group..."
-echo "(2) Creating LDAP User..."
-echo "(3) Creating Kerberos User..."
-echo "(4) Creating HDFS Home Dir..."
-echo "(5) Setting HDFS Home Dir's Quota..."
-echo "(6) Creating Hive Upload Dir..."
-echo "(7) Setting Hive Upload Dir's Quota..."
-echo "(8) Setting Hive Upload Dir's ACL..."
-echo "(9) Creating Hive DB, Role..."
+echo "(01) Creating LDAP Group..."
+echo "(02) Creating LDAP User..."
+echo "(03) Creating Kerberos User..."
+echo "(04) Creating HDFS Home Dir..."
+echo "(05) Setting HDFS Home Dir's Quota..."
+echo "(06) Creating Hive Upload Dir..."
+echo "(07) Setting Hive Upload Dir's Quota..."
+echo "(08) Setting Hive Upload Dir's ACL..."
+echo "(09) Creating Hive DB, Role..."
+echo "(10) Setting Hive DB's Quota..."
 echo "Press Any Key to Continue..."
 stty_orig=`stty -g`
 stty -echo
@@ -29,7 +30,7 @@ read USERPASS
 stty $stty_orig
 # 
 echo "+-------------------------------------------------------------------+"
-echo "(1) Creating LDAP Group..."
+echo "(01) Creating LDAP Group..."
 SVR=tfxa008
 RET=`ssh $SVR "sh $SCRIPTDIR/addldapgroup.sh $PROJECT $DESCRIP" 2>/dev/null`
 GID=`echo $RET|cut -d ' ' -f6|cut -d '=' -f2`
@@ -41,7 +42,7 @@ read USERPASS
 stty $stty_orig
 #
 echo "+-------------------------------------------------------------------+"
-echo "(2) Creating LDAP User..."
+echo "(02) Creating LDAP User..."
 SVR=tfxa008
 RET=`ssh $SVR "sh $SCRIPTDIR/addldapuser.sh $GID $PROJECT" 2>/dev/null`
 UNUM=`echo $RET|cut -d ' ' -f7|cut -d '=' -f2`
@@ -53,7 +54,7 @@ read USERPASS
 stty $stty_orig
 #
 echo "+-------------------------------------------------------------------+"
-echo "(3) Creating Kerberos User..."
+echo "(03) Creating Kerberos User..."
 RET=`sh $SCRIPTDIR/addkdcuser.sh $PROJECT $UNUM 2>/dev/null`
 PRINC=`echo $RET|cut -d ' ' -f8|cut -d "\"" -f2`
 echo "Project ${PROJECT} 's Kerberos principal=[${PRINC}], pwd=[$UNUM]"
@@ -64,7 +65,7 @@ read USERPASS
 stty $stty_orig
 #
 echo "+-------------------------------------------------------------------+"
-echo "(4) Creating HDFS Home Dir..."
+echo "(04) Creating HDFS Home Dir..."
 SVR=tf2p079
 DIRNAME="/user/$PROJECT"
 ssh $SVR "echo $HDFSKDCPWD|kinit $HDFSKDCPRINC >/dev/null;sh $SCRIPTDIR/mkhdfsdir.sh $DIRNAME $PROJECT" 2>/dev/null
@@ -77,7 +78,7 @@ read USERPASS
 stty $stty_orig
 #
 echo "+-------------------------------------------------------------------+"
-echo "(5) Setting HDFS Home Dir's Quota..."
+echo "(05) Setting HDFS Home Dir's Quota..."
 SVR=tf2p079
 DIRNAME="/user/$PROJECT"
 ssh $SVR "echo $HDFSKDCPWD|kinit $HDFSKDCPRINC >/dev/null;sh $SCRIPTDIR/sethdfsquota.sh $DIRNAME $QUOTA" 2>/dev/null
@@ -90,7 +91,7 @@ read USERPASS
 stty $stty_orig
 #
 echo "+-------------------------------------------------------------------+"
-echo "(6) Creating Hive Upload Dir..."
+echo "(06) Creating Hive Upload Dir..."
 SVR=tf2p079
 DIRNAME="/hive/${PROJECT}_upload"
 ssh $SVR "echo $HDFSKDCPWD|kinit $HDFSKDCPRINC >/dev/null;sh $SCRIPTDIR/mkhdfsdir.sh $DIRNAME hive" 2>/dev/null
@@ -103,7 +104,7 @@ read USERPASS
 stty $stty_orig
 #
 echo "+-------------------------------------------------------------------+"
-echo "(7) Setting Hive Upload Dir's Quota..."
+echo "(07) Setting Hive Upload Dir's Quota..."
 SVR=tf2p079
 DIRNAME="/hive/${PROJECT}_upload"
 ssh $SVR "echo $HDFSKDCPWD|kinit $HDFSKDCPRINC >/dev/null;sh $SCRIPTDIR/sethdfsquota.sh $DIRNAME $QUOTA" 2>/dev/null
@@ -116,7 +117,7 @@ read USERPASS
 stty $stty_orig
 #
 echo "+-------------------------------------------------------------------+"
-echo "(8) Setting Hive Upload Dir's ACL..."
+echo "(08) Setting Hive Upload Dir's ACL..."
 SVR=tf2p079
 DIRNAME="/hive/${PROJECT}_upload"
 ssh $SVR "echo $HDFSKDCPWD|kinit $HDFSKDCPRINC >/dev/null;sh $SCRIPTDIR/sethdfsacl.sh $DIRNAME 2>/dev/null default:group:$PROJECT:rwx,group:$PROJECT:rwx"
@@ -129,7 +130,7 @@ read USERPASS
 stty $stty_orig
 #
 echo "+-------------------------------------------------------------------+"
-echo "(9) Creating Hive DB, Role..."
+echo "(09) Creating Hive DB, Role..."
 SVR=tf2p079
 JDBCURL="jdbc:hive2://tf2p077.cht.local:10000/default;principal=hive/tf2p077.cht.local@CHT.COM.TW"
 ssh $SVR "echo $HDFSKDCPWD|kinit $HDFSKDCPRINC >/dev/null;sh $SCRIPTDIR/createdb.sh \"$JDBCURL\" $PROJECT" 2>/dev/null
@@ -140,6 +141,19 @@ echo "Listing Role $ROLENAME..."
 ssh $SVR "echo $HDFSKDCPWD|kinit $HDFSKDCPRINC >/dev/null;beeline --silent=true -u \"$JDBCURL\" -e \"show grant role $ROLENAME\"" 2>/dev/null
 echo "Listing Group $PROJECT..."
 ssh $SVR "echo $HDFSKDCPWD|kinit $HDFSKDCPRINC >/dev/null;beeline --silent=true -u \"$JDBCURL\" -e \"show role grant group $PROJECT\"" 2>/dev/null
+#
+echo "+-------------------------------------------------------------------+"
+echo "(10) Setting Hive DB's Quota..."
+SVR=tf2p079
+DIRNAME="/user/hive/warehouse/$PROJECT.db"
+ssh $SVR "echo $HDFSKDCPWD|kinit $HDFSKDCPRINC >/dev/null;sh $SCRIPTDIR/sethdfsquota.sh $DIRNAME $QUOTA" 2>/dev/null
+echo "Listing Hive DB's Quota..."
+RET=`ssh $SVR "echo $HDFSKDCPWD|kinit $HDFSKDCPRINC;hdfs dfs -count -q $DIRNAME" 2>/dev/null`;echo $RET|cut -d' ' -f11,6,7
+echo "Press Any Key to Continue..."
+stty_orig=`stty -g`
+stty -echo
+read USERPASS
+stty $stty_orig
 #
 echo "+-------------------------------------------------------------------+"
 echo "All are done"
